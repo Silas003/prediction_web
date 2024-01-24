725daa1e3f64cf3c3245d2571cd3c5b656b609dd
silas003 <silaskumi4@gmail.com>
2024-01-24 01:04:34 +0000

    static

--- predictor/settings.py
+++ predictor/settings.py
@@ -1,16 +1,17 @@
 import os
-
+import dj_database_url
 from pathlib import Path
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env=environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b!p126@bq8f)rufn0^#_!=!yczv33px3-ou^syt(2^j+tp7*2u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,11 +71,15 @@ WSGI_APPLICATION = 'predictor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+    'default':dj_database_url.parse(env('EXT_DATABASE_URL'))
 }
 
 
