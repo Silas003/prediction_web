379058eb71943b5b43487c465a56d3d0d03c2382
silas003 <silaskumi4@gmail.com>
2024-01-26 15:05:35 +0000

    debug

--- predictor/settings.py
+++ predictor/settings.py
@@ -71,16 +71,16 @@ WSGI_APPLICATION = 'predictor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES={
-    'default':dj_database_url.parse(env('EXT_DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES={
+#     'default':dj_database_url.parse(env('EXT_DATABASE_URL'))
+# }
 
 
 # Password validation
--- services/models.py
+++ services/models.py
@@ -12,6 +12,7 @@ class Fixture(models.Model):
     team1=models.CharField(max_length=100)
     team2=models.CharField(max_length=100)
     League=models.CharField(max_length=100,default='EPL')
+    tip=models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.team1} vs {self.team2}  results=={self.outcome}'
+        return f'{self.team1} vs {self.team2}   results - {self.outcome}'
